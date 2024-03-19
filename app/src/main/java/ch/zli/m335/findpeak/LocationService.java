1e77cd016892f393dd3be83958ed94b5c5e5a534
Patrick Côté <85751624+cotpat@users.noreply.github.com>
2024-03-19 13:39:00 +0100

    mod: removed experimental code mod: moved sensor initialization to onBind add: magnetometer callback

--- app/src/main/java/ch/zli/m335/findpeak/LocationService.java
+++ app/src/main/java/ch/zli/m335/findpeak/LocationService.java
@@ -3,8 +3,6 @@ package ch.zli.m335.findpeak;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -20,16 +18,13 @@ public class LocationService extends Service implements SensorEventListener {
     private final IBinder binder = new LocationBinder();
 
     private SensorManager sensorManager;
+
     private Sensor magnetometer;
-    private Sensor accelerometer;
+    private float[] magnetometerValues = new float[3];
+    private CompassCallback compassCallback;
+
     private Sensor location;
 
-    private float[] lastAccelerometer = new float[3];
-    private float[] lastMagnetometer = new float[3];
-    private boolean lastAccelerometerSet = false;
-    private boolean lastMagnetometerSet = false;
-    private float[] rotationMatrix = new float[9];
-    private float[] orientation = new float[3];
 
     public LocationService() { }
 
@@ -39,18 +34,17 @@ public class LocationService extends Service implements SensorEventListener {
         }
     }
 
-    @Override
-    public void onCreate() {
-        super.onCreate();
-
-
-        initializeCompassSensor();
-
-    }
-
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
+        if (magnetometer != null) {
+            sensorManager.registerListener(
+                    this,
+                    magnetometer,
+                    SAMPLING_RATE
+            );
+        }
+
         return binder;
     }
 
@@ -58,59 +52,23 @@ public class LocationService extends Service implements SensorEventListener {
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
         magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-    }
-
-    public Sensor getMagnetometer() {
-        return magnetometer;
-    }
-
-    public Sensor getAccelerometer() {
-        return accelerometer;
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if (event.sensor == magnetometer) {
-            System.arraycopy(
-                    event.values,
-                    0,
-                    lastMagnetometer,
-                    0,
-                    event.values.length
-            );
-            lastMagnetometerSet = true;
-        } else if (event.sensor == accelerometer) {
-            System.arraycopy(
-                    event.values,
-                    0,
-                    lastAccelerometer,
-                    0,
-                    event.values.length
-            );
-            lastAccelerometerSet = true;
-        }
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+            magnetometerValues = event.values;
 
-        if (lastMagnetometerSet && lastAccelerometerSet) {
-            SensorManager.getRotationMatrix(
-                    rotationMatrix,
-                    null,
-                    lastAccelerometer,
-                    lastMagnetometer
-            );
-            SensorManager.getOrientation(rotationMatrix, orientation);
-
-            float azimuthInRadians = orientation[0];
-            float azimuthInDegrees = (float) (Math.toDegrees(azimuthInRadians) + 360) % 360;
-
-            Bitmap originalBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.compass);
+            if (compassCallback != null) {
+                compassCallback.onCompassUpdate(magnetometerValues);
+            }
         }
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    public void onAccuracyChanged(Sensor sensor, int accuracy) { }
 
+    public interface CompassCallback {
+        void onCompassUpdate(float[] magneticValues);
     }
-
-
 }
