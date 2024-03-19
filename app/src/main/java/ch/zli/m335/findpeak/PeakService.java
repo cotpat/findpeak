19e7375e230f4f8db602613748ba50c36d3498f6
Patrick Côté <85751624+cotpat@users.noreply.github.com>
2024-03-19 13:28:18 +0100

    mod: moved CSV opening and parsing to own method mod: moved CSV opening and parsing to onBind add: some debugging messages

--- app/src/main/java/ch/zli/m335/findpeak/PeakService.java
+++ app/src/main/java/ch/zli/m335/findpeak/PeakService.java
@@ -2,18 +2,18 @@ package ch.zli.m335.findpeak;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
 
 public class PeakService extends Service {
     private final IBinder binder = new PeakBinder();
@@ -29,31 +29,41 @@ public class PeakService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.d("PeakService", "service started");
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        readAndParseCsvFile();
+        return binder;
+    }
+
+    private void readAndParseCsvFile() {
+        AssetManager am = getApplicationContext().getAssets();
+        InputStream is = null;
 
         try {
-            InputStream is = getAssets().open("peaks.csv");
-            List<String> records = Files.readAllLines(Paths.get(is.toString()), StandardCharsets.UTF_8);
+            is = am.open("peaks.csv");
+            BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
 
-            for (String record : records) {
-                String[] fields = record.split(";");
+            Log.d("PeakService", "peaks.csv opened");
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] fields = line.split(";");
 
                 new PeakModel(
                         Integer.parseInt(fields[0]),
                         fields[1],
                         Double.parseDouble(fields[2]),
                         Double.parseDouble(fields[3])
-                        );
-                Log.d("CREATE:", fields[1]);
+                );
+                Log.d("CREATE", fields[1]);
             }
 
+            br.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
-
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        return binder;
     }
 }
